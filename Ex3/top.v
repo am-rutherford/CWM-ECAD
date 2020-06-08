794f21994c05595e20db72d202d4b722dea3ea68
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-08 14:25:11 +0000

    Ex3 potential solution

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name: Alexander Rutherford
+// Date: 08/06/2020
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
@@ -20,11 +20,26 @@
 
 module counter(
     //Todo: add ports 
-
+    input clk,
+    input rst,
+    input enable,
+    input direction,
+    output [7:0] counter_out
     );
                     
     //Todo: add registers and wires, if needed
+    reg [7:0]count;
+
 
     //Todo: add user logic
+    assign counter_out = count;
+
+    always @(posedge clk or posedge rst) begin
+	if(rst)
+	  count <= 8'b0000_0000;
+	else begin
+	  count <= (enable == 0) ? count : (direction) ? (count + 1): (count - 1);
+	end
+    end  
       
 endmodule
