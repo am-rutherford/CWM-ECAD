8490a2dfe1240f7e111772828b8480f2d1366e85
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-08 16:04:44 +0000

    Ex3 still in progression

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -22,6 +22,7 @@ module top_tb(
     reg rst;
     reg enable;
     reg [7:0] counter_out;
+    reg [7:0] counter_prev;
     reg err;
 
 //Todo: Clock generation
@@ -34,14 +35,42 @@ module top_tb(
 
 //Todo: User logic
     initial begin
+    counter_out = 0;
+    counter_prev = counter_out;
     err = 0;
     rst = 0;
     direction = 1;
+    enable = 1;
+
+      forever begin
+      	//Test whether counter = 0 when reset = 1
+	if (rst&(counter_out != 0)) begin
+	  err = 1;
+          $display("***TEST FAILED! counter==%d when reset==%d, enable==%d ***",counter_out,rst,enable);
+	end
+
+	if (direction&(counter_out != (counter_prev+1))| !direction&(counter_out != (counter_prev-1))) begin
+	  err = 1;
+	  $display("***TEST FAILED! counter==%d and counter_prev=%d when direction=%d ***",counter_out,counter_prev,direction);
+	end
+
+	if (!enable&(counter_out != counter_prev))s
+	  err = 1;
+	  $display("***TEST FAILED! counter==%d and counter_prev=%d when enable=%d ***",counter_out,counter_prev,enable);
+	end
+	
+    end
     
 //Todo: Finish test, check for success
+      initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
 
 //Todo: Instantiate counter module
 count counter(clk, rst, enable, direction, counter_out);
-
 
 endmodule 
