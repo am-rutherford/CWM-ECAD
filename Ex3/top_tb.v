360dcd43db1836ef1f8341dd006da67c9984b905
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-09 08:26:42 +0000

    Ex3 in progress

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -49,16 +49,21 @@ module top_tb(
           $display("***TEST FAILED! counter==%d when reset==%d, enable==%d ***",counter_out,rst,enable);
 	end
 
-	if (direction&(counter_out != (counter_prev+1))| !direction&(counter_out != (counter_prev-1))) begin
+	if (direction&(counter_out != (counter_prev+1)) | !direction&(counter_out != (counter_prev-1))) begin
 	  err = 1;
 	  $display("***TEST FAILED! counter==%d and counter_prev=%d when direction=%d ***",counter_out,counter_prev,direction);
 	end
 
-	if (!enable&(counter_out != counter_prev))s
+	if (!enable&(counter_out != counter_prev)) | (enable&(counter_out == counter_prev) begin
 	  err = 1;
 	  $display("***TEST FAILED! counter==%d and counter_prev=%d when enable=%d ***",counter_out,counter_prev,enable);
 	end
-	
+      	counter_prev = counter_out;
+      	if (counter_out == 0)
+	  direction = ~direction;
+      	end
+      end
+
     end
     
 //Todo: Finish test, check for success
