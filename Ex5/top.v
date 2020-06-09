4cb15004aefbd1310260c02dc588db56ca14a6b9
Cloud User <centos@ip-172-31-6-222.eu-west-2.compute.internal>
2020-06-09 16:00:17 +0000

    Ex5 in progress

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Alexander Rutherford
+// Date: 09/06/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,32 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module lights(
+	input clk,
+	output red,
+	output amber,
+	output green
+	);
+
+	// define wires and registers
+	reg [2:0] light
+	
+
+	// logic
+	assign red = light[2];
+	assign amber = light[1];
+	assign green = light[0];
+
+	always @(posedge clk) begin
+		case (light)
+		3'b100 : light <= 3'b110;
+		3'b110 : light <= 3'b001;
+		3'b001 : light <= 3'b001;
+		default : light <= 3'b100
+		endcase
+
+	end
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Alexander Rutherford
+// Date: 09/06/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    	);
+
+	//parameters
+    	parameter CLK_PERIOD = 10;
+
+	//registers and wires
+    	reg clk;
+	reg err;
+    	reg [2:0] light;
+	wire [2:0] throw;
+	reg [2:0] throw_prev;
+	
